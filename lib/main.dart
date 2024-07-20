8b8bb5398595b1000bc5fce2d946758076b67f6e
Victor Chirinos <vchirinosb@gmail.com>
2024-07-20 10:20:49 -0500

    mi primer widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp());
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+///Page View Screen
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
 }
